a42383788caf234e151b7f92f6611d2c6612c278
Haocheng Li <daniel.hc.li@gmail.com>
2020-09-09 14:34:53 -0700

    Added villager spawn command

--- me/Daniel/AdvancedVillagersPlugin/DebugCommands/DebugCommand.java
+++ me/Daniel/AdvancedVillagersPlugin/DebugCommands/DebugCommand.java
@@ -1,9 +1,17 @@
 package me.Daniel.AdvancedVillagersPlugin.DebugCommands;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import me.Daniel.AdvancedVillagersPlugin.Master;
 
@@ -24,9 +32,12 @@ public class DebugCommand implements CommandExecutor {
 	 * */
 	public DebugCommand(Master plugin) {
 		this.plugin = plugin;
-		
+
 		// connect "hello" to this class
 		plugin.getCommand("hello").setExecutor(this);
+		
+		// connect "villager" to this class
+		plugin.getCommand("villager").setExecutor(this);
 		
 	}
 	
@@ -41,6 +52,22 @@ public class DebugCommand implements CommandExecutor {
 				Player p = (Player) sender;
 				p.sendMessage("Hello there!");
 				
+				return true;
+			} 
+		}
+		
+
+		// check if command is villager
+		if (label.equalsIgnoreCase("villager")) {
+			// check if command came from player, not console
+			if (sender instanceof Player) {
+				Player p = (Player) sender;
+				p.sendMessage("Villager spawned!");
+				Villager villager = (Villager) p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.VILLAGER);
+				LivingEntity villagerLE = (LivingEntity) villager;
+				villagerLE.getEquipment().setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+				PotionEffect regen = new PotionEffect(PotionEffectType.HEALTH_BOOST, 90000, 2);
+				villager.addPotionEffect(regen);
 				return true;
 			} 
 		}
